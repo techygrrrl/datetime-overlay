7da5c1d333afc75e0908e70da525a2aba19e6550
techygrrrl <techygrrrl@proton.me>
2024-01-25 04:06:06 -0500

    date & time display w/ date-fns

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import './style.css'
+import { formatDate } from "date-fns";
+
+document.addEventListener('DOMContentLoaded', () => {
+  initDateTime()
+})
+
+function initDateTime() {
+  const element = document.querySelector<HTMLDivElement>('#app')!
+
+  const perform = () => {
+    const dt = new Date()
+
+    const date = formatDate(dt, 'dd MMM y')
+    const amPm = formatDate(dt, 'bbb')
+    const time = formatDate(dt, 'h:mm')
+
+    element.innerHTML = `
+      <div class="clock">
+        <span class="clock__time">
+          ${time}<span class="clock__am-pm">${amPm}</span>
+        </span>
+        <span class="clock__date">
+          ${date}
+        </span>
+      </div>
+    `
+  }
+
+  perform()
+
+  setInterval(() => {
+    perform()
+  }, 10000)
+}
